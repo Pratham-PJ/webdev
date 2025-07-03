efbef45beb77a645c82bee6c47e8eb7f9ecba82e
Pratham-PJ <prathamjoshi356@gmail.com>
2025-07-03 17:01:47 +0530

    compteted setting up front end and making signup page

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
@@ -13,6 +14,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true //allow front end to send cookies
+}));
 app.use(express.json());
 // it is important for our onboarding page
 // using this we can access cookies inside middleware.js
